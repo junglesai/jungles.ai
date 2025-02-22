c8de16566ae416773dbe9e498bc1f15b868c6388
aijungledev <remaginet.com@gmail.com>
2025-02-22 19:29:27 +0200

    update featured img

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -209,7 +209,7 @@ function App() {
               <div className="max-w-7xl mx-auto">
                 <div className="flex justify-center items-center">
                   <img src={judgeRight} alt="AI Debates Logo" className="w-20 mb-2 mr-4" /> 
-                  <h1 className="md:text-5xl font-bold text-yellowgreen-100 text-center uppercase courier">
+                  <h1 className="md:text-5xl font-bold text-yellowgreen-100 text-center uppercase courier md:mt-2">
                     <span className='text-2xl md:text-5xl' style={{lineHeight: '1'}}>AI-Powered</span> <span className='text-4xl md:text-5xl' style={{lineHeight: '1'}}>Debates</span>
                   </h1>
                   <img src={judgeLogo} alt="AI Debates Logo" className="w-20 mb-2 ml-4" /> 
