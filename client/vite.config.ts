593e6cafe56cb3443e7593b871b5a88c290f82d0
junglesdev <remaginet.com@gmail.com>
2025-04-01 16:58:36 +0300

    use vite preview in prod

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
     }
   },
   server: {
-    host: '0.0.0.0',
     port: 5173,
+    host: true,
     watch: {
       usePolling: true,
       interval: 100
@@ -21,6 +21,10 @@ export default defineConfig({
     hmr: {
       overlay: false
     }
+  },
+  preview: {
+    host: true,
+    port: 5173
   },
   css: {
     postcss: './postcss.config.js'
